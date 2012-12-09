a04901e2d44e71d737ef9ab1570f134d18d4635f
min <min@min-laptop.(none)>
2012-12-09 11:18:35 +0800

    Initial importing of the source code

---
+++ src/IncrementAtomicLong.java
@@ -0,0 +1,22 @@
+public class IncrementAtomicLong {
+
+  private static AtomicLong value;
+  private static long counter;
+
+  public static void main(String[] args) throws Exception {
+    Thread.sleep(5000);
+    value = new AtomicLong(0);
+    for(counter = 0; counter < 500000000L; counter++) {
+      value.getAndIncrement();
+    }
+
+    value.set(0);
+    long start = System.nanoTime();
+    for(counter =0; counter < 500000000L; counter++) {
+      value.getAndIncrement();
+    }
+
+    System.out.println("Cost time: " + (System.nanoTime() - start));
+
+  }
+}
